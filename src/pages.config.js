abd70c6387646eea1f2a2f450e9137b3f1405ae4
Morgan0787 <007capral007@gmail.com>
2026-03-19 16:18:50 +0500

    Update pages.config.js

--- src/pages.config.js
+++ src/pages.config.js
@@ -2,19 +2,18 @@ import AdminDataQuality from './pages/AdminDataQuality';
 import AdminFeedback from './pages/AdminFeedback';
 import EssayChecker from './pages/EssayChecker';
 import Home from './pages/Home';
+import Login from './pages/Login';
 import Profile from './pages/Profile';
 import Recommendations from './pages/Recommendations';
 import Search from './pages/Search';
 import __Layout from './Layout.jsx';
-import Login from './pages/Login';
-   // and in PAGES object:
-   "Login": Login,
 
 export const PAGES = {
     "AdminDataQuality": AdminDataQuality,
     "AdminFeedback": AdminFeedback,
     "EssayChecker": EssayChecker,
     "Home": Home,
+    "Login": Login,
     "Profile": Profile,
     "Recommendations": Recommendations,
     "Search": Search,
@@ -24,4 +23,4 @@ export const pagesConfig = {
     mainPage: "Home",
     Pages: PAGES,
     Layout: __Layout,
-}; 
+};
